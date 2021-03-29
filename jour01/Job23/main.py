879f06246071cc8e990f092c6e789f1d6c6a45b7
julia-denivet <julia.denivet@laplateforme.io>
2021-03-29 17:47:25 +0200

    jour01/Job23/main.py Non fini

---
+++ jour01/Job23/main.py
@@ -0,0 +1,32 @@
+def draw_rectangle(width, height):
+    
+    i=0 
+
+    while i <= width:
+        print("-", end="") 
+        i+=1
+    
+    
+
+    a = 0
+   
+
+    while a <= height:
+        print( "|")
+        a+=1
+
+ 
+
+   
+
+print(draw_rectangle(10, 3))
+
+
+
+        
+
+
+
+
+
+
